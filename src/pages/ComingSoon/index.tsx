e9e872c3a57a294d7b4b3717aced28db0ca17e96
Gabriel Brunichaki <gabriel.brunichaki@triider.com>
2020-08-26 18:52:16 -0300

    Coming soon page

---
+++ src/pages/ComingSoon/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const ComingSoon: React.FC = () => {
+  return (
+    <S.Wrapper>
+      <S.Heading>Coming Soon...</S.Heading>
+    </S.Wrapper>
+  );
+};
+
+export default ComingSoon;
